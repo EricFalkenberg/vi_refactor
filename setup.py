951332fc988f5eb08233f2967c933c1f2de0609d
Eric Falkenberg <eric_falkenberg@intuit.com>
2017-10-06 17:29:13 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@
 from setuptools import setup
 
 setup(name="Vi Refactor",
-    version="0.0.2",
+    version="0.0.3",
     description="CLI tool for refactoring your codebase with vi/m.",
     author="Eric Falkenberg",
     author_email="exf4789@rit.edu",
     url="https://ericfalkenberg.github.io/vi_refactor/",
-    scripts=['vi_refactor']
+    scripts=['vi_refactor'],
+    data_files=[('.vi_refactor', ['.vi_refactor'])],
 )  
