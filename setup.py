358c1613a8d36371cd6abb99d5319e96d532ef87
Eric Falkenberg <eric_falkenberg@intuit.com>
2017-10-07 11:42:03 -0700

    setup program for use with click as it works a bit better than argparse config file no longer needed. setup file altered and ensured working

--- setup.py
+++ setup.py
@@ -2,12 +2,18 @@
 
 from setuptools import setup
 
-setup(name="Vi Refactor",
+setup(name="vi_refactor",
     version="0.0.3",
     description="CLI tool for refactoring your codebase with vi/m.",
     author="Eric Falkenberg",
     author_email="exf4789@rit.edu",
     url="https://ericfalkenberg.github.io/vi_refactor/",
-    scripts=['vi_refactor'],
-    data_files=[('.vi_refactor', ['.vi_refactor'])],
+    py_modules=['vi_refactor'],
+    install_requires=[
+        'click',
+    ],
+    entry_points='''
+        [console_scripts]
+        vi_refactor=vi_refactor:cli
+    ''',
 )  
