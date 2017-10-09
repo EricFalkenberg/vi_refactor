59930532d415f10b99ce4805435437aacebbce84
EricFalkenberg <exf4789@rit.edu>
2017-10-09 15:57:35 -0700

    add means to compare code blocks

--- src/click_cmd/analyze.py
+++ src/click_cmd/analyze.py
@@ -1,4 +1,6 @@
 import javalang
+import hashlib
+import pickle
 
 TEST_SNIPPET = """
 package com.falkenberg.eric;
@@ -12,14 +14,27 @@ class JustSayHello {
 }
 """
 
+TEST_SNIPPET_2 = """
+package com.falkenberg.eric;
+
+class JustSayHello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
+"""
+
+def compare_code_blocks(block1, block2):
+    return obj_to_hash_val(block1) == obj_to_hash_val(block2)
+
+def obj_to_hash_val(obj):
+    raw = pickle.dumps(obj)
+    return hashlib.sha256(raw).hexdigest()
+
 if __name__ == '__main__':
-    tree = javalang.parse.parse(TEST_SNIPPET)
-    print tree.package.name
-    print tree.types[0].name
-    for dec in tree.types[0].body:
-        out_string = ""
-        print dec.name, dec.throws
-        for p in dec.parameters:
-            print p.type.name, p.type.dimensions, p.name, p.varargs
-        print dec.body[0].expression.arguments
-        print dec.return_type
+    tree1 = javalang.parse.parse(TEST_SNIPPET)
+    tree2 = javalang.parse.parse(TEST_SNIPPET_2)
+
+    print compare_code_blocks(tree1, tree2)
